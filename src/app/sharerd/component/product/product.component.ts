d2c38bffae92e66d289c64ba02b2ffcf929b2563
pradhanshri <pshripad10@gmail.com>
2023-05-01 01:11:49 +0530

    Directive *ngIf

--- src/app/sharerd/component/product/product.component.ts
+++ src/app/sharerd/component/product/product.component.ts
@@ -10,14 +10,24 @@ export class ProductComponent implements OnInit {
   pName: string = "Samsung M31";
   pId: number = 123;
   pStatus: string ="InTransition";
-  isDisabled: boolean =true
+  isDisabled: boolean =true;
+  isProdAvalaible !: boolean
+
   constructor() { }
 
   ngOnInit(): void {
-
+    this.isProdAvalaible = Math.random() >= 0.5 ? true : false;
     setTimeout(() => {
       this.isDisabled=false;
     }, 4000);
   }
 
+  getColor(){
+   if(this.isProdAvalaible){
+    return 'orange';
+   }else{
+    return '#ccc'
+   }
+
+  }
 }
